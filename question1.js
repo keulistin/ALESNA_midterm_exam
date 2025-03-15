b369d113474c63a39b3a385b900222ca1d8367c5
unknown <YY502@ccs.local>
2025-03-15 14:14:54 +0800

    added some comments for question1.js

--- question1.js
+++ question1.js
@@ -1,6 +1,6 @@
-function sumArray(numbers) {
+function sumArray(numbers) { //we have here our function in an array
     return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-}
+} //using the reduce method,it iterates over each element in the array
 
 
-console.log(sumArray([1, 2, 3, 4, 5])); 
+console.log(sumArray([1, 2, 3, 4, 5])); //output
